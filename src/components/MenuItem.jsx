69560a3e072aa34d07d0c1ddf0f9a0e86824670e
eleazarmercader <elmercaderdesundberg@gmail.com>
2023-11-24 19:44:38 +0100

    CSS changes on MenuItem

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -13,23 +13,30 @@ function MenuItem({ item }) {
     };
 
     return (
-        <div className="card">
+        <div className="card text-center">
             <img src={item.image} alt={item.name} className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
-                <p className="card-text">${item.price}</p>
-                <Link to={`/detail/${item.id}`} className="btn btn-primary">
-                    More Info
+                <Link to={`/detail/${item.id}`} className="info-btn">
+                    Description
                 </Link>
-                {!isAdded ? (
-                    <button onClick={handleAddToCart} className="btn btn-secondary">
-                        Add to Cart
-                    </button>
-                ) : (
-                    <button onClick={handleAddToCart} className="btn btn-success">
-                        Add another one
-                    </button>
-                )}
+                <p className="card-text font-semibold mt-2">CHF {item.price}</p>
+                <div className='flex justify-around'>
+                    {!isAdded ? (
+                        <button onClick={handleAddToCart} className="add-btn">
+                            Add to Cart
+                        </button>
+                    ) : (
+                        <div>
+                            <button onClick={handleAddToCart} className="add-btn">
+                                Add another one
+                            </button>
+                            <Link to={`/detail/${item.id}`} className="ml-4 info-btn">
+                                Go to cart
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
